4bc433934328237068a06b712e6d66901b9dcd00
omondimaurine <omondimaurine713@gmail.com>
2024-04-25 06:58:21 +0300

    handling lists and keys

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,13 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const display = links.map((link,index)=>(
+    <a key={index} href={'#'+ link}> {link}</a>
+  ))
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (<nav>{/* display an <a> tag for each link here */}
+  {display}
+  </nav>);
 }
 
 export default NavBar;
